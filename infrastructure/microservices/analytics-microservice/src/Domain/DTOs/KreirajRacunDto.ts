fa4ba9f5307cfed7ae7e3033ac1c68460ed98f80
Tatjana <Tatjana@DESKTOP-6Q43N4J>
2026-01-05 11:43:35 +0100

    promena kreirajRacunDto

--- infrastructure/microservices/analytics-microservice/src/Domain/DTOs/KreirajRacunDto.ts
+++ infrastructure/microservices/analytics-microservice/src/Domain/DTOs/KreirajRacunDto.ts
@@ -1,5 +1,11 @@
+import { TipProdaje, NacinPlacanja } from "../enums/ProdajaEnums";
+
 export interface KreirajRacunDto {
   datum?: string; 
+
+  tipProdaje?: TipProdaje; 
+  nacinPlacanja?: NacinPlacanja;
+
   stavke: Array<{
     parfemNaziv: string;
     kolicina: number;
